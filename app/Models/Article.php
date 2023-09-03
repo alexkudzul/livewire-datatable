0fb7e25982137494bc0d0fb3870fbf425f89f464
alexkudzul <alexkudzul@gmail.com>
2023-09-03 15:55:11 -0600

    Defining relationships in Eloquent

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    // Relación uno a uno o muchos inversa
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
